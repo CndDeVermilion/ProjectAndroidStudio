32b9ee8aa4164f0995629f7b47fbe1c2cd6e9593
C <ycandoxs05@gmail.com>
2024-12-25 21:31:05 +0700

    Add files via upload

---
+++ java/com/example/project1lusianisa/Frame2Layout.java
@@ -0,0 +1,40 @@
+package com.example.project1lusianisa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class Frame2Layout extends AppCompatActivity {
+    EditText eUserName;
+    ImageButton bLogin;
+    String user;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frame2_layout);
+
+        eUserName = findViewById(R.id.username);
+        bLogin = findViewById(R.id.login);
+
+        bLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login();
+            }
+        });
+    }
+
+    public void login(){
+        user=eUserName.getText().toString();
+        Intent intent = new Intent(Frame2Layout.this,LinearActivity2.class);
+
+        intent.putExtra("user",user);
+        Toast.makeText(this, "selamat datang"+user, Toast.LENGTH_LONG).show();
+        startActivity(intent);
+    }
+}
---
+++ java/com/example/project1lusianisa/LinearActivity3.java
@@ -0,0 +1,14 @@
+package com.example.project1lusianisa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LinearActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear3);
+    }
+}
